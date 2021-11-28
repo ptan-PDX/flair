01fe8c0f0b2cccd4952e27adfd425effa29b7010
ptan-PDX <ptan@pdx.edu>
2021-11-27 23:29:45 -0800

    changed LASER

--- flair/embeddings/document.py
+++ flair/embeddings/document.py
@@ -33,44 +33,87 @@ class DocumentEmbeddings(Embeddings):
 class LASEREmbeddings(DocumentEmbeddings):
     def __init__(
             self,
-            train_dataset,
-            **vectorizer_params,
+            embeddings: List[TokenEmbeddings],
+            fine_tune_mode: str = "none",
+            pooling: str = "mean",
     ):
-        """The constructor for DocumentTFIDFEmbeddings.
-        :param train_dataset: the train dataset which will be used to construct vectorizer
-        :param vectorizer_params: parameters given to Scikit-learn's TfidfVectorizer constructor
+        """The constructor takes a list of embeddings to be combined.
+        :param embeddings: a list of token embeddings
+        :param fine_tune_mode: if set to "linear" a trainable layer is added, if set to
+        "nonlinear", a nonlinearity is added as well. Set this to make the pooling trainable.
+        :param pooling: a string which can any value from ['mean', 'max', 'min']
         """
         super().__init__()
 
-        import numpy as np
-        self.vectorizer = TfidfVectorizer(dtype=np.float32, **vectorizer_params)
-        self.vectorizer.fit([s.to_original_text() for s in train_dataset])
+        self.embeddings: StackedEmbeddings = StackedEmbeddings(embeddings=embeddings)
+        self.__embedding_length = self.embeddings.embedding_length
 
-        self.__embedding_length: int = len(self.vectorizer.vocabulary_)
+        # optional fine-tuning on top of embedding layer
+        self.fine_tune_mode = fine_tune_mode
+        if self.fine_tune_mode in ["nonlinear", "linear"]:
+            self.embedding_flex = torch.nn.Linear(
+                self.embedding_length, self.embedding_length, bias=False
+            )
+            self.embedding_flex.weight.data.copy_(torch.eye(self.embedding_length))
+
+        if self.fine_tune_mode in ["nonlinear"]:
+            self.embedding_flex_nonlinear = torch.nn.ReLU(self.embedding_length)
+            self.embedding_flex_nonlinear_map = torch.nn.Linear(
+                self.embedding_length, self.embedding_length
+            )
+
+        self.__embedding_length: int = self.embeddings.embedding_length
 
         self.to(flair.device)
 
-        self.name: str = f"document_tfidf"
+        if pooling not in ['min', 'max', 'mean']:
+            raise ValueError(f"Pooling operation for {self.mode!r} is not defined")
+
+        self.pooling = pooling
+        self.name: str = f"document_{self.pooling}"
 
     @property
     def embedding_length(self) -> int:
         return self.__embedding_length
 
     def embed(self, sentences: Union[List[Sentence], Sentence]):
-        """Add embeddings to every sentence in the given list of sentences."""
+        """Add embeddings to every sentence in the given list of sentences. If embeddings are already added, updates
+        only if embeddings are non-static."""
 
         # if only one sentence is passed, convert to list of sentence
         if isinstance(sentences, Sentence):
             sentences = [sentences]
 
-        raw_sentences = [s.to_original_text() for s in sentences]
-        tfidf_vectors = torch.from_numpy(self.vectorizer.transform(raw_sentences).A)
+        self.embeddings.embed(sentences)
 
-        for sentence_id, sentence in enumerate(sentences):
-            sentence.set_embedding(self.name, tfidf_vectors[sentence_id])
+        for sentence in sentences:
+            word_embeddings = []
+            for token in sentence.tokens:
+                word_embeddings.append(token.get_embedding().unsqueeze(0))
+
+            word_embeddings = torch.cat(word_embeddings, dim=0).to(flair.device)
+
+            if self.fine_tune_mode in ["nonlinear", "linear"]:
+                word_embeddings = self.embedding_flex(word_embeddings)
+
+            if self.fine_tune_mode in ["nonlinear"]:
+                word_embeddings = self.embedding_flex_nonlinear(word_embeddings)
+                word_embeddings = self.embedding_flex_nonlinear_map(word_embeddings)
+
+            if self.pooling == "mean":
+                pooled_embedding = torch.mean(word_embeddings, 0)
+            elif self.pooling == "max":
+                pooled_embedding, _ = torch.max(word_embeddings, 0)
+            elif self.pooling == "min":
+                pooled_embedding, _ = torch.min(word_embeddings, 0)
+
+            sentence.set_embedding(self.name, pooled_embedding)
 
     def _add_embeddings_internal(self, sentences: List[Sentence]):
         pass
+
+    def extra_repr(self):
+        return f"fine_tune_mode={self.fine_tune_mode}, pooling={self.pooling}"
 
 
 #PTAN
