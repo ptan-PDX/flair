3c2b0f06519946d8ff422dde7e770e9017c1d35e
ptan-PDX <ptan@pdx.edu>
2021-11-27 23:06:25 -0800

    ugh

--- flair/embeddings/document.py
+++ flair/embeddings/document.py
@@ -243,7 +243,7 @@ class TransformerDocumentEmbeddings(DocumentEmbeddings):
     def embedding_length(self) -> int:
         """Returns the length of the embedding vector."""
         return (
-            len(self.layer_indexes) * self.model.config.hidden_size
+            len(self.layer_indexes + 2) * (self.model.config.hidden_size + 21)
             if not self.layer_mean
             else self.model.config.hidden_size + 1000
         )
